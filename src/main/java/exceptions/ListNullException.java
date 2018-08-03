36a6cdb36a566231dd2d1b2a7ac5051ea51ba6aa
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-04 00:04:04 +0300

    Added list rotation

---
+++ src/main/java/exceptions/ListNullException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class ListNullException extends RuntimeException{
+
+    private String _message;
+
+    public ListNullException(String message){
+        _message = message;
+    }
+
+    @Override
+    public String getMessage(){
+        return _message;
+    }
+}
--- src/main/java/rotation/ListRotation.java
+++ src/main/java/rotation/ListRotation.java
@@ -1,4 +1,49 @@
 package rotation;
 
-public class ListRotation {
+import Enums.Direction;
+import exceptions.ArrayNullException;
+import exceptions.ListNullException;
+
+import java.util.List;
+
+public class ListRotation<T> {
+    public static <T> void rotate(List<T> list, Direction direction, int rotateCount) throws ListNullException {
+
+        if(list == null ) throw new ArrayNullException("Array is null");
+        int length = list.size();
+
+        if(length == 0) return;
+        int count = rotateCount % length;
+        if(count == 0) return;
+
+        int directionCount = getCountRelatedDirection(direction, count, length);
+
+        rotationInner(list, directionCount, length);
+    }
+
+    private static int getCountRelatedDirection(Direction direction, int count, int length){
+        int localCount = count;
+        if(direction == Direction.RIGHT){
+            localCount = length - count;
+        }
+        return localCount;
+    }
+
+    private static <T> void rotationInner(List<T> list, int directionCount, int length){
+        System.out.println(directionCount);
+        rotateArrayInner(list, 0, directionCount-1);
+        rotateArrayInner(list, directionCount, length-1);
+        rotateArrayInner(list, 0, length-1);
+    }
+
+    private static <T> void rotateArrayInner(List<T> list, int start, int end){
+
+        while(start < end){
+            T temp = list.get(start);
+            list.set(start, temp);
+            list.set(end, temp);
+            start++;
+            end--;
+        }
+    }
 }
