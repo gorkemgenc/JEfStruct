2c408b6cb673f40fcbc2897d0c7ff9160fed809d
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-06 23:30:24 +0300

    Added list sort

--- src/main/java/sorting/ListSort.java
+++ src/main/java/sorting/ListSort.java
@@ -1,4 +1,65 @@
 package sorting;
 
-public class ListSort {
+import java.util.List;
+
+public class ListSort<T> {
+    public static void descendingOrder(List<Integer>[] list){
+
+    }
+
+    public static void ascendingOrder(List<Integer>[] list){
+
+    }
+
+    public static <T> void sortByLength(List<T>[] list) {
+
+    }
+
+    public static void alphabeticalCharOrder(List<Character> list){
+
+    }
+
+    public static void alphabeticalOrder(List<String> list){
+
+    }
+
+    public static void reverseAlphabeticalCharOrder(List<Character> list){
+
+    }
+
+    public static void reverseAlphabeticalOrder(List<String> list){
+
+    }
+
+    public static <T> void orderBySpecial(List<T> list, String areaName){
+
+    }
+
+    public static <T> void orderBySpecials(List<T>, List<String> areanames){
+
+    }
+
+    public static void alphabeticalOrderWithSubString(List<String> list){
+
+    }
+
+    public static <T> void bubbleSort(List<T> list){
+
+    }
+
+    public static <T> void mergeSort(List<T> list){
+
+    }
+
+    public static <T> void quickSort(List<T> list){
+
+    }
+
+    public static <T> void insertionSort(List<T> list){
+
+    }
+
+    public static <T> void heapSort(List<T> list){
+
+    }
 }
