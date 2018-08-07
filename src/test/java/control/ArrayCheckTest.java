d51e5f434b3d413eb5f63b9e439a62a561bbed5c
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-07 14:26:48 +0300

    Implemented check of array

---
+++ src/test/java/control/ArrayCheckTest.java
@@ -0,0 +1,16 @@
+package control;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayCheckTest {
+
+    @Test
+    public void isUnique() {
+    }
+
+    @Test
+    public void permutations() {
+    }
+}
