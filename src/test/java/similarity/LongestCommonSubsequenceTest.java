34ce1da85b9e4828092a3d747820919fec579f74
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-07 11:33:10 +0300

    Added lcs

---
+++ src/test/java/similarity/LongestCommonSubsequenceTest.java
@@ -0,0 +1,13 @@
+package similarity;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LongestCommonSubsequenceTest extends TestCase {
+
+    @Test
+    public void longestCommonSubsequence() {
+    }
+}
