6e3d9bcfa60cdc5c7b57d5143f6e04eda98ade3c
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-07 11:27:00 +0300

    Added Jackard similarity

---
+++ src/test/java/similarity/JaccardSimilarityTest.java
@@ -0,0 +1,17 @@
+package similarity;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JaccardSimilarityTest extends TestCase {
+
+    @Test
+    public void jaccardSimilarity() {
+    }
+
+    @Test
+    public void jaccardSimilarityForList() {
+    }
+}
