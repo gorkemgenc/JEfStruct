47b289884189632c2ca535e701538f3d2e1e881b
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-08 13:52:49 +0300

    Refactoring

--- src/main/java/transformation/ArrayTransformation.java
+++ src/main/java/transformation/ArrayTransformation.java
@@ -1,8 +1,20 @@
 package transformation;
 
+import java.math.BigInteger;
+
 public class ArrayTransformation {
 
-    public static <T> toBigInt(T)
+    public static <T> BigInteger[] toBigInt(T[] array){
+        return null;
+    }
+
+    public static <T> int[] toInt(T[] array){
+
+    }
+
+    public static <T> String[] toString(T[] array){
+
+    }
     //Array - > BigInt to String, Int to String, String to Int, String to Char, Int to BigInt, String to BigInt with default array optional List
 
     //BigInt to String, Int to String array , String to Int array, BigInt to String array, String to BigInt Array with default array optional List or Array
--- src/main/java/transformation/IntegerTransformation.java
+++
@@ -1,5 +0,0 @@
-package transformation;
-
-public class IntegerTransformation {
-    // find permutation and total divisible points
-}
