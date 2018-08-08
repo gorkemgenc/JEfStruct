8f74eae61c7ee9b47b82b321f340f273df1e6d0a
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-08 13:21:35 +0300

    Improvements

--- src/main/java/control/StringCheck.java
+++ src/main/java/control/StringCheck.java
@@ -1,11 +1,33 @@
 package control;
 
+import java.util.HashSet;
+
 public class StringCheck {
     private static boolean isUnique(String str){
+        HashSet<Character> set = new HashSet<>();
+
+        for(int i = 0; i<str.length(); i++){
+            if(set.contains(str.charAt(i))) return false;
+            set.add(str.charAt(i));
+        }
         return true;
     }
 
     private static String deduplication(String str){
-        return null;
+        if(str == null) return null;
+
+        int length = str.length();
+        HashSet<Character> set = new HashSet<>();
+        StringBuilder builder = new StringBuilder();
+
+        for(int i=0; i<str.length(); i++){
+            Character c = str.charAt(i);
+
+            if(!set.contains(c)){
+                builder.append(c);
+                set.add(c);
+            }
+        }
+        return builder.toString();
     }
 }
