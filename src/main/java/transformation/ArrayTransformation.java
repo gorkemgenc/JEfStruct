fdb71db17ebd526470822e6d7da66b31001ead49
gorkem.genc <m.gorkemgenc@outlook.com>
2018-08-08 14:04:07 +0300

    Added transformation files

--- src/main/java/transformation/ArrayTransformation.java
+++ src/main/java/transformation/ArrayTransformation.java
@@ -9,15 +9,10 @@ public class ArrayTransformation {
     }
 
     public static <T> int[] toInt(T[] array){
-
+        return null;
     }
 
     public static <T> String[] toString(T[] array){
-
+        return null;
     }
-    //Array - > BigInt to String, Int to String, String to Int, String to Char, Int to BigInt, String to BigInt with default array optional List
-
-    //BigInt to String, Int to String array , String to Int array, BigInt to String array, String to BigInt Array with default array optional List or Array
-    //type ı check et list is onu yap array ise onu yap reflection
-    //StringtoInt with deleting other characters
 }
--- src/main/java/transformation/ListTransformation.java
+++ src/main/java/transformation/ListTransformation.java
@@ -1,4 +1,19 @@
 package transformation;
 
+import java.math.BigInteger;
+import java.util.List;
+
 public class ListTransformation {
+
+    public static <T> BigInteger[] toBigInt(List<T> list){
+        return null;
+    }
+
+    public static <T> int[] toInt(List<T> list){
+        return null;
+    }
+
+    public static <T> String[] toString(List<T> list){
+        return null;
+    }
 }
